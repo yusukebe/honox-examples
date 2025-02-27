454eb6d212cede67d3b59c1bad4361abf9b0dbce
Yusuke Wada <yusuke@kamawada.com>
2025-02-27 11:40:35 +0900

    simplify

--- projects/tailwind/vite.config.ts
+++ projects/tailwind/vite.config.ts
@@ -3,19 +3,14 @@ import honox from 'honox/vite'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => {
-  if (mode === 'client') {
-    return {
-      build: {
-        manifest: true,
-        rollupOptions: {
-          input: ['./app/style.css']
-        }
-      },
-      plugins: [tailwindcss()]
-    }
-  }
-  return {
-    plugins: [tailwindcss(), honox(), build()]
-  }
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    honox({
+      client: {
+        input: ['./app/style.css']
+      }
+    }),
+    build()
+  ]
 })
